08e0ed8622a8dced56a397196026a631c981ca03
Mariia Riabinina <m.riabinina@mobidev.biz>
2022-11-28 12:29:28 +0200

    added new project of courses

---
+++ layouts/404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-12 bg-red-300 w-full h-full min-h-screen flex flex-col items-center justify-center pb-56">
+    <slot />
+  </div>
+</template>
---
+++ pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="font-bold text-gray-700 text-9xl mb-10">404</h1>
+  <p class="text-3xl mb-10">I guess that page doesn't exist.</p>
+  <NuxtLink
+      class="rounded text-white font-bold py-2 px-4 cursor-pointer bg-green-500"
+      to='/'
+  >
+    Go back Home
+  </NuxtLink>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: '404'
+})
+</script>
